500da27f7932f28601f2be5b89f34cd1c5278942
Andy Driver <andy.driver@digital.justice.gov.uk>
2015-03-20 16:31:00 +0000

    Order results by distance from point

--- laalaa/apps/advisers/serializers.py
+++ laalaa/apps/advisers/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
+from rest_framework_gis import serializers as gis_serializers
 
-from .models import Office
+from .models import Location
 
 
-class OfficeSerializer(serializers.HyperlinkedModelSerializer):
+class DistanceField(serializers.ReadOnlyField):
+    def to_representation(self, obj):
+        # miles
+        return obj.mi
+
+
+class LocationSerializer(gis_serializers.GeoModelSerializer):
+    distance = DistanceField()
+
     class Meta:
-        model = Office
-        fields = ('organisation', 'account_number', 'telephone', 'location')
+        model = Location
+        fields = (
+            'address', 'city', 'postcode', 'point', 'organisation',
+            'location_type', 'distance')
--- laalaa/apps/advisers/views.py
+++ laalaa/apps/advisers/views.py
@@ -1,9 +1,10 @@
+from django.contrib.gis.geos import Point
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from rest_framework import viewsets
 
-from .models import Office
-from .serializers import OfficeSerializer
+from .models import Location
+from .serializers import LocationSerializer
 
 
 class Search(TemplateView):
@@ -11,5 +12,16 @@ class Search(TemplateView):
 
 
 class AdviserViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Office.objects.all()
-    serializer_class = OfficeSerializer
+    serializer_class = LocationSerializer
+
+    def get_queryset(self):
+        lon = self.request.query_params.get('lon')
+        lat = self.request.query_params.get('lat')
+        pnt = Point(-0.12776, 51.50735)
+
+        try:
+            pnt = Point(float(lon), float(lat))
+        except (ValueError, TypeError):
+            pass
+
+        return Location.objects.all().distance(pnt).order_by('distance')
