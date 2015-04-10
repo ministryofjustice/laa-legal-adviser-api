a971d823fbf4157b6c7a113f9d888719eac650c9
Andy Driver <andy.driver@digital.justice.gov.uk>
2015-04-10 13:25:08 +0100

    Add static urls to app

--- laalaa/urls.py
+++ laalaa/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from advisers.admin import admin_site
 
@@ -6,4 +8,4 @@ from advisers.admin import admin_site
 urlpatterns = patterns('',
     url(r'^admin/', include(admin_site.urls)),
     url(r'^', include('advisers.urls')),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
