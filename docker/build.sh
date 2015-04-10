40d8ab40fdf15ec87caad0e52caf15d62dbda13a
Peter Idah <peter.idah@gmail.com>
2015-04-10 11:14:05 +0100

    check /v1/_ping which doesn't require authentication.

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,7 @@ APP_NAME='laalaa'
 DOCKER='docker'
 (( EUID == 0 )) || DOCKER='sudo docker'
 
-[[ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' https://$DOCKER_REGISTRY/v1/repositories/$APP_NAME/images) == '200' ]] || { echo -e "\nTHE DOCKER REGISTRY IS DOWN!"; exit 1; }
+[[ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' https://$DOCKER_REGISTRY/v1/_ping) == '200' ]] || { echo -e "\nTHE DOCKER REGISTRY IS DOWN!"; exit 1; }
 
 $DOCKER build --force-rm=true -t $DOCKER_REGISTRY/$APP_NAME .
 $DOCKER tag $DOCKER_REGISTRY/$APP_NAME $DOCKER_REGISTRY/$APP_NAME:$GIT_COMMIT
