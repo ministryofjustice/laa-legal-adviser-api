d4d245528b97db1e15aaeb4b3afffc2bec0834b6
Josh Rowe <josh@s-block.com>
2015-10-14 15:08:20 +0100

    delete old data before migrating models

--- laalaa/apps/advisers/migrations/0007_auto_20151014_1428.py
+++ laalaa/apps/advisers/migrations/0007_auto_20151014_1428.py
@@ -1,7 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
+
 from django.db import models, migrations
+from django.core import serializers
+
+
+fixture_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../fixtures'))
+fixture_filename = 'initial_advisers.json'
+
+
+def load_fixture(apps, schema_editor):
+    fixture_file = os.path.join(fixture_dir, fixture_filename)
+
+    fixture = open(fixture_file, 'rb')
+    objects = serializers.deserialize('json', fixture, ignorenonexistent=True)
+    for obj in objects:
+        obj.save()
+    fixture.close()
+
+
+def unload_fixture(apps, schema_editor):
+    "Brutally deleting all entries for this model..."
+
+    for m in ['location', 'organisationtype', 'outreachtype', 'category']:
+        Mod = apps.get_model("advisers", m)
+        Mod.objects.all().delete()
 
 
 class Migration(migrations.Migration):
@@ -11,6 +36,10 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunSQL("TRUNCATE advisers_location RESTART IDENTITY CASCADE;"),
+        migrations.RunSQL("TRUNCATE advisers_organisationtype RESTART IDENTITY CASCADE;"),
+        migrations.RunSQL("TRUNCATE advisers_outreachtype RESTART IDENTITY CASCADE;"),
+        migrations.RunSQL("TRUNCATE advisers_category RESTART IDENTITY CASCADE;"),
         migrations.AlterField(
             model_name='office',
             name='location',
@@ -23,4 +52,5 @@ class Migration(migrations.Migration):
             field=models.OneToOneField(null=True, to='advisers.Location'),
             preserve_default=True,
         ),
+        migrations.RunPython(load_fixture, reverse_code=unload_fixture),
     ]
