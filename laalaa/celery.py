4583e5a3f3f1a81de7273f6e9e939ab951db7bc4
Josh Rowe <josh@s-block.com>
2015-11-04 10:41:36 +0000

    update celery file for sentry logging

--- laalaa/celery.py
+++ laalaa/celery.py
@@ -2,10 +2,13 @@
 from __future__ import absolute_import
 
 import os
+from raven.contrib.celery import register_logger_signal
+from raven.scripts.runner import send_test_message
 
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laalaa.settings')
 
 from django.conf import settings  # noqa
@@ -17,7 +20,19 @@ app = Celery('laalaa')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
+client = None
+if hasattr(settings, 'RAVEN_CONFIG'):
+    from raven.contrib.django.models import client
+    register_logger_signal(client)
+    register_logger_signal(client)
+
 
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+@app.task()
+def sentry_test_task():
+    if client:
+        send_test_message(client, {})
