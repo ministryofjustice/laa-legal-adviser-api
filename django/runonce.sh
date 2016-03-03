abc36fa6ff306138d098328a0f69e53a405a39ba
James Narey <james.narey@digital.justice.gsi.gov.uk>
2016-03-03 21:36:27 +0000

    Restructure to bring closer to master branch

--- django/runonce.sh
+++
@@ -1,6 +0,0 @@
-!#/bin/bash
-
-PGPASSWORD=$POSTGRES_PASSWORD /usr/bin/psql --host $POSTGRES_HOST -U $POSTGRES_USER $POSTGRES_USER -f create_db.sql
-PGPASSWORD=$POSTGRES_PASSWORD /usr/bin/psql --host $POSTGRES_HOST -d laalaa -U $POSTGRES_USER $POSTGRES_USER -f create_extensions.sql
-python /home/app/manage.py migrate
-python /home/app/manage.py createsuperuser --username=admin
