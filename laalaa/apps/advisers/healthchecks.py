22821d276ae9a5584baa50e425495038a680cfe0
Nick Rowlands <nick@rowlando.com>
2018-08-31 15:31:48 +0100

    Use django-moj-irat for the ping.json

--- laalaa/apps/advisers/healthchecks.py
+++ laalaa/apps/advisers/healthchecks.py
@@ -5,7 +5,7 @@ from moj_irat.healthchecks import HealthcheckResponse, JsonUrlHealthcheck, \
 
 registry.register_healthcheck(UrlHealthcheck(
     name='postcodes.io',
-    url='%(base_url)s/postcxodes/SW1A1AA' % {
+    url='%(base_url)s/postcodes/SW1A1AA' % {
         'base_url': settings.POSTCODES_IO_URL,
     },
     headers={'Content-Type': 'application/json'},
--- laalaa/urls.py
+++ laalaa/urls.py
@@ -4,10 +4,14 @@ from django.conf.urls.static import static
 
 from advisers.admin import admin_site
 from moj_irat.views import PingJsonView, HealthcheckView
-import healthcheck.views
 
 urlpatterns = [
-    url(r'^ping.json$', healthcheck.views.ping),
+    url(r'^ping.json$', PingJsonView.as_view(
+        build_tag_key='APP_BUILD_TAG',
+        build_date_key='APP_BUILD_DATE',
+        commit_id_key='APP_GIT_COMMIT',
+        version_number_key='APPVERSION',
+    ), name='ping_json'),
     url(r'^healthcheck.json$', HealthcheckView.as_view(), name='healthcheck_json'),
     url(r'^admin/', include(admin_site.urls)),
     url(r'^', include('advisers.urls'))
