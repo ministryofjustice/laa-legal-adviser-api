f0c730f1546f3f58014e78ac4d5c52fc1b6e51d7
Nick Rowlands <nick@rowlando.com>
2018-09-03 12:56:12 +0100

    Never cache ping.json

--- laalaa/urls.py
+++ laalaa/urls.py
@@ -7,12 +7,12 @@ from advisers.admin import admin_site
 from moj_irat.views import PingJsonView, HealthcheckView
 
 urlpatterns = [
-    url(r'^ping.json$', PingJsonView.as_view(
+    url(r'^ping.json$', never_cache(PingJsonView.as_view(
         build_tag_key='APP_BUILD_TAG',
         build_date_key='APP_BUILD_DATE',
         commit_id_key='APP_GIT_COMMIT',
         version_number_key='APPVERSION',
-    ), name='ping_json'),
+    )), name='ping_json'),
     url(r'^healthcheck.json$', never_cache(HealthcheckView.as_view()), name='healthcheck_json'),
     url(r'^admin/', include(admin_site.urls)),
     url(r'^', include('advisers.urls'))
