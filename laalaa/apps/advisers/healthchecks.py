9e4aaef78b2f5fdbee235c4f311d920e93f37275
Nick Rowlands <nick@rowlando.com>
2018-09-03 17:10:54 +0100

    Extends the healthcheck with a "messaging" check
    The possible error messages are:
    
    - No running workers were found
    - Check that the message broker server is running
    - No workers running

--- laalaa/apps/advisers/healthchecks.py
+++ laalaa/apps/advisers/healthchecks.py
@@ -1,10 +1,43 @@
 from django.conf import settings
 from moj_irat.healthchecks import HealthcheckResponse, UrlHealthcheck, registry
 
+class CeleryWorkersHealthcheck(object):
+    def __init__(self, name):
+        self.name = name
+    
+    def __call__(self, *args, **kwargs):
+        from celery import Celery
+        
+        try:
+            app = Celery('laalaa')
+            stats = app.control.inspect().stats()
+            if not stats:
+                return self.error_response('No running workers were found.')
+            workers = stats.values()
+            if len(workers) == 0:
+                return self.error_response('No workers running')
+        except IOError as e:
+            msg = str(e)
+            msg += '. Check that the message broker is running.'
+            return self.error_response(msg)
+        except ImportError as e:
+            return self.error_response(str(e))
+        return self.success_response()
+        
+    def error_response(self, error):
+        return HealthcheckResponse(self.name, False, error=error)
+    
+    def success_response(self):
+        return HealthcheckResponse(self.name, True)
+
 registry.register_healthcheck(UrlHealthcheck(
     name='postcodes.io',
     url='%(base_url)s/postcodes/SW1A1AA' % {
         'base_url': settings.POSTCODES_IO_URL,
     },
     headers={'Content-Type': 'application/json'},
+))
+        
+registry.register_healthcheck(CeleryWorkersHealthcheck(
+    name='workers'
 ))
