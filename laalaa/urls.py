fc74fc34b5ffe0fe5190b73df786c31b0b790197
Nick Rowlands <nick@rowlando.com>
2018-09-07 12:33:09 +0100

    Simplify ping.json (#63)

--- laalaa/urls.py
+++ laalaa/urls.py
@@ -4,15 +4,9 @@ from django.conf.urls.static import static
 from django.views.decorators.cache import never_cache
 
 from advisers.admin import admin_site
-from moj_irat.views import PingJsonView, HealthcheckView
+from moj_irat.views import HealthcheckView
 
 urlpatterns = [
-    url(r'^ping.json$', never_cache(PingJsonView.as_view(
-        build_tag_key='APP_BUILD_TAG',
-        build_date_key='APP_BUILD_DATE',
-        commit_id_key='APP_GIT_COMMIT',
-        version_number_key='APPVERSION',
-    )), name='ping_json'),
     url(r'^healthcheck.json$', never_cache(HealthcheckView.as_view()), name='healthcheck_json'),
     url(r'^admin/', include(admin_site.urls)),
     url(r'^', include('advisers.urls'))
