d5f8eb8fa026c6b8e326def4b7c45459f9cf487d
Nick Rowlands <nick@rowlando.com>
2018-09-07 21:38:49 +0100

    Remove redundant code

--- laalaa/apps/advisers/healthchecks.py
+++ laalaa/apps/advisers/healthchecks.py
@@ -6,28 +6,29 @@ class CeleryWorkersHealthcheck(object):
         self.name = name
     
     def __call__(self, *args, **kwargs):
-        import os
         from celery import Celery
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laalaa.settings')
         from django.conf import settings
         
         try:
             app = Celery('laalaa')
             app.config_from_object('django.conf:settings')
-            app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
             stats = app.control.inspect().stats()
+
             if not stats:
                 return self.error_response('No running workers were found.')
+            
             workers = stats.values()
             if not workers:
                 return self.error_response('No workers running.')
+
         except IOError as e:
             msg = str(e)
             msg += '. Check that the message broker is running.'
             return self.error_response(msg)
+
         except ImportError as e:
             return self.error_response(str(e))
+            
         return self.success_response()
         
     def error_response(self, error):
