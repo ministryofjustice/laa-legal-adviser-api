270d091c6f78d68352e4fd956bfa752b22b8ee59
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2019-05-13 17:57:07 +0100

    Configure database from rds secret
     - replace use of the 'db' secret with the 'rds' secret
     - use the key names used by terraform
     - configure DB_NAME and DB_USERNAME separately and use accordingly in
    settings and the bash script

--- laalaa/settings/base.py
+++ laalaa/settings/base.py
@@ -99,7 +99,7 @@ TEMPLATES = [
 DATABASES = {
     "default": {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": os.environ.get("DB_USERNAME", "laalaa"),
+        "NAME": os.environ.get("DB_NAME", "laalaa"),
         "USER": os.environ.get("DB_USERNAME", "postgres"),
         "PASSWORD": os.environ.get("DB_PASSWORD", ""),
         "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
