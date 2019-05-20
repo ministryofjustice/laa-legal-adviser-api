d9ab0f6cb966897a9913766ea4f0a1196d510761
said-moj <said.shire@digital.justice.gov.uk>
2019-05-20 10:22:22 +0100

    Export UWSGI_CONFIG_FILE environment variable

--- docker/run.sh
+++ docker/run.sh
@@ -12,6 +12,6 @@ if [ "$ENV" != "prod" ]; then
 fi
 
 # Run server
-UWSGI_CONFIG_FILE=${UWSGI_CONFIG_FILE:-/home/app/conf/uwsgi.ini:allinone}  # Until we move off template deploy and onto Kubernetes with the split app
+export UWSGI_CONFIG_FILE=${UWSGI_CONFIG_FILE:-/home/app/conf/uwsgi.ini:allinone}  # Until we move off template deploy and onto Kubernetes with the split app
 export WORKER_APP_CONCURRENCY=${WORKER_APP_CONCURRENCY:-8}
 /usr/local/bin/uwsgi --ini $UWSGI_CONFIG_FILE
