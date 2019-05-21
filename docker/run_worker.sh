9cb4aaa762f472b80381a6ec0880108492df965b
said-moj <said.shire@digital.justice.gov.uk>
2019-05-21 09:16:12 +0100

    Fix typo in run_worker script

--- docker/run_worker.sh
+++ docker/run_worker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -e
-exec ./manage.py celery celery worker -A laalaa --concurrency=$WORKER_APP_CONCURRENCY --loglevel=INFO
+exec ./manage.py celery worker -A laalaa --concurrency=$WORKER_APP_CONCURRENCY --loglevel=INFO
