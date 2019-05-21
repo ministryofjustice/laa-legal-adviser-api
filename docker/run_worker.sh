f39894f48e5341454aef60fdd1e67bd99d77000e
said-moj <said.shire@digital.justice.gov.uk>
2019-05-21 10:46:22 +0100

    Using docker/run_worker.sh for template-deploy as well

--- docker/run_worker.sh
+++ docker/run_worker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -e
-exec ./manage.py celery worker -A laalaa --concurrency=$WORKER_APP_CONCURRENCY --loglevel=$LOG_LEVEL
+exec ./manage.py celery worker -A laalaa --concurrency=$WORKER_APP_CONCURRENCY --loglevel=$LOG_LEVEL $*
