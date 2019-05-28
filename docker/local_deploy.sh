dd43634803696da86d674695d29030223f0c610d
said-moj <said.shire@digital.justice.gov.uk>
2019-05-28 13:55:34 +0100

    Adding comment for keeping development replicaset low

--- docker/local_deploy.sh
+++ docker/local_deploy.sh
@@ -1,2 +1,4 @@
-#!/bin/sh -xe
-kubectl config use-context docker-for-desktop && ECR_DEPLOY_IMAGE=laalaa_local .circleci/deploy_to_kubernetes development
+#!/usr/bin/env bash
+set -e
+kubectl config use-context docker-for-desktop
+ECR_DEPLOY_IMAGE=laalaa_local .circleci/deploy_to_kubernetes development
