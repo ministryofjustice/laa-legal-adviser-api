4652deceb8d46162fad46c8d26329c78808e3cea
said-moj <said.shire@digital.justice.gov.uk>
2019-05-29 11:03:23 +0100

    Create shared services deployment for development namespace to mock external services

--- docker/local_deploy.sh
+++ docker/local_deploy.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -e
+
+ROOT=$(dirname "$0")
+NAMESPACE="development"
+NAMESPACE_DIR="$ROOT/../kubernetes_deploy/$NAMESPACE"
+
 kubectl config use-context docker-for-desktop
-ECR_DEPLOY_IMAGE=laalaa_local .circleci/deploy_to_kubernetes development
+
+# Shared services deployment, only available for development to mock external services
+kubectl apply --record=false --filename=$NAMESPACE_DIR/shared_services_deployment.yml
+
+ECR_DEPLOY_IMAGE=laalaa_local .circleci/deploy_to_kubernetes $NAMESPACE
