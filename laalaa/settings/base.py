5c71f92ab5c3a6a1b597cb0b2ae0e2f2a33997ae
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2019-05-31 16:13:56 +0100

    Get AWS s3 region from s3 secret

--- laalaa/settings/base.py
+++ laalaa/settings/base.py
@@ -135,7 +135,7 @@ if os.environ.get("STATIC_FILES_BACKEND") == 's3':
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_S3_REGION_NAME = 'eu-west-2'
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
 
 STATIC_URL = "/static/"
 STATIC_ROOT = root("static")
