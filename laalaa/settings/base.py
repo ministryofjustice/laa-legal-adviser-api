2f94e3e2b17552183f619db86aceb514e33de2ea
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2019-05-31 13:46:28 +0100

    Specify v4 signature for s3

--- laalaa/settings/base.py
+++ laalaa/settings/base.py
@@ -135,6 +135,7 @@ if os.environ.get("STATIC_FILES_BACKEND") == 's3':
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+S3_USE_SIGV4 = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = root("static")
