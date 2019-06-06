49935a1ac1f3870d4a2dc40d15a0f84198d096fa
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2019-06-06 15:50:54 +0100

    Set AWS default ACL
    The default in django storages is to make the files public, this change
    makes them private

--- laalaa/settings/base.py
+++ laalaa/settings/base.py
@@ -136,6 +136,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+AWS_DEFAULT_ACL = None
 
 STATIC_URL = "/static/"
 STATIC_ROOT = root("static")
