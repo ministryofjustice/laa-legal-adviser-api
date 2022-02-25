a8165673ffd0d4b38ce57fb594bbfd27e8dc87fb
said-moj <said.shire@digital.justice.gov.uk>
2022-02-25 12:20:20 +0000

    Remove celery status check from worker pod health checks
    This command timesout on the new kubernetes live cluster. A --timeout option was added to the status on later version of celery

--- docker/workers_healthcheck.sh
+++ docker/workers_healthcheck.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
 exec ./manage.py worker_health_checks
-exec celery -A laalaa status
+# This timeouts on the new cluster and there is no way to pass the --timeout option in this version of celery
+# exec celery -A laalaa status
