efd3709752e632df876cbbe927e80579b40b6202
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2022-12-08 13:54:34 +0000

    LGA-2296: Update run_worker script to use global -A arg for celery 5+.

--- docker/run_worker.sh
+++ docker/run_worker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-exec celery worker -A laalaa --concurrency=${WORKER_APP_CONCURRENCY:-4} --loglevel=${LOG_LEVEL:-INFO}
+exec celery -A laalaa worker --concurrency=${WORKER_APP_CONCURRENCY:-4} --loglevel=${LOG_LEVEL:-INFO}
