9d91c1c2bd3d3f2d58d40c6e4283946b149773e0
charles <chalston.pinnock@justice.gov.uk>
2026-07-06 13:30:17 +0100

    adding a dummy root to test deployment

--- laalaa/apps/advisers/urls.py
+++ laalaa/apps/advisers/urls.py
@@ -7,5 +7,6 @@ from categories.views import CategoryViewSet
 router = routers.DefaultRouter()
 router.register(r"legal-advisers", views.AdviserViewSet, basename="legal-advisers")
 router.register(r"categories_of_law", CategoryViewSet, basename="categories")
+router.register(r"dummy_root", CategoryViewSet, basename="categories")
 
 urlpatterns = [re_path(r"^", include(router.urls))]
